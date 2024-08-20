074a810dec3137343f40a774c7147ab1abde9acb
Bexst24 <aakashgujar2001@gmail.com>
2024-08-20 12:09:11 +0530

    Built header layout

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,14 +1,18 @@
+import Header from "@/components/header";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
-  return <>
-  
-  <main>
-    <header></header>
-    <Outlet/>
-  </main>
-  <footer></footer>
-  </>;
+  return (
+    <>
+      <main className="min-h-screen container">
+        <Header/>
+        <Outlet />
+      </main>
+      <footer className="p-10 text-center bg-gray-800 mt-10">
+        Credit goes to RSC 🙌 for the tutorial and valuable content.
+      </footer>
+    </>
+  );
 };
 
 export default AppLayout;
